bde81c8e21db0cc9faa7a286728df0f97693ab8d
Alif rendy <alifrendy663@gmail.com>
2023-01-06 09:53:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import Content1 from './components/Content1'
+import Content2 from './components/Content2'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Content1/>
+      <Content2/>
     </div>
   );
 }
---
+++ src/components/Content2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content2 = () => {
+  return (
+    <div>
+        <div className='line'></div>
+    </div>
+  )
+}
+
+export default Content2
